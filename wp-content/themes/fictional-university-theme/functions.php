494e991dfdeacc7de8e021f1765c458df9fefec3
connorjnel <connor.j.nel00@gmail.com>
2023-02-07 14:30:46 +0000

    convert static html to wp

--- wp-content/themes/fictional-university-theme/functions.php
+++ wp-content/themes/fictional-university-theme/functions.php
@@ -2,7 +2,11 @@
 
 // Load main stylesheet
 function uni_theme_files() {
-    wp_enqueue_style( 'uni_theme_mainstyle', get_stylesheet_uri());
+    wp_enqueue_style( 'google-font', '//fonts.googleapis.com/css2?family=Roboto+Condensed&display=swap');
+    wp_enqueue_style( 'font-awesome', '//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css');
+    wp_enqueue_style( 'uni_theme_main_style', get_theme_file_uri('/build/style-index.css'));
+    wp_enqueue_style( 'uni_theme_extra_style', get_theme_file_uri('/build/index.css'));
+
 }
 
 // Fire/Call functions
