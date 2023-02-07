8c71a9796cba146dfaa8952d2716c62ba78f3e97
connorjnel <connor.j.nel00@gmail.com>
2023-02-07 15:08:46 +0000

    formatting fix prettier

--- wp-content/themes/fictional-university-theme/functions.php
+++ wp-content/themes/fictional-university-theme/functions.php
@@ -1,13 +1,13 @@
-<?php 
+<?php
 
-// Load main stylesheet
-function uni_theme_files() {
-    wp_enqueue_script( 'uni_theme_js', get_theme_file_uri('/build/index.js'), array('jquery'), '1.0', true);
-    wp_enqueue_style( 'google_font', '//fonts.googleapis.com/css2?family=Roboto+Condensed&display=swap');
-    wp_enqueue_style( 'font_awesome', '//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css');
-    wp_enqueue_style( 'uni_theme_main_style', get_theme_file_uri('/build/style-index.css'));
-    wp_enqueue_style( 'uni_theme_extra_style', get_theme_file_uri('/build/index.css'));
-
+// Load stylesheets, js scripts, font files
+function uni_theme_files()
+{
+    wp_enqueue_script('uni_theme_js', get_theme_file_uri('/build/index.js'), array('jquery'), '1.0', true);
+    wp_enqueue_style('google_font', '//fonts.googleapis.com/css2?family=Roboto+Condensed&display=swap');
+    wp_enqueue_style('font_awesome', '//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css');
+    wp_enqueue_style('uni_theme_main_style', get_theme_file_uri('/build/style-index.css'));
+    wp_enqueue_style('uni_theme_extra_style', get_theme_file_uri('/build/index.css'));
 }
 
 // Fire/Call functions
--- wp-content/themes/fictional-university-theme/single.php
+++ wp-content/themes/fictional-university-theme/single.php
@@ -1,17 +1,17 @@
 <!-- Single Post Template -->
 
-<?php 
+<?php
 
-    get_header(); 
+get_header();
 
-    while(have_posts()) {
-        the_post(); ?>
-        <h2><?php the_title(); ?> </h2>
-        <p><?php the_content(); ?></p>
-        <small><?php the_author(); ?></small>
-        <?php
-    }
+while (have_posts()) {
+    the_post(); ?>
+    <h2><?php the_title(); ?> </h2>
+    <p><?php the_content(); ?></p>
+    <small><?php the_author(); ?></small>
+<?php
+}
 
-    get_footer();
+get_footer();
 
 ?>
