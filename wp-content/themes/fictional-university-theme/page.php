61a48a7904c71d05b6ed3e28fa9a9ab8a6d73367
connorjnel <connor.j.nel00@gmail.com>
2023-02-07 16:59:53 +0000

    parent and child pages

--- wp-content/themes/fictional-university-theme/page.php
+++ wp-content/themes/fictional-university-theme/page.php
@@ -19,11 +19,20 @@ while (have_posts()) {
     </div>
 
     <div class="container container--narrow page-section">
-        <div class="metabox metabox--position-up metabox--with-home-link">
-            <p>
-                <a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main"><?php the_title() ?></span>
-            </p>
-        </div>
+
+        <?php
+        if (wp_get_post_parent_id(get_the_ID()) == True) { ?>
+
+            <div class="metabox metabox--position-up metabox--with-home-link">
+                <p><a class="metabox__blog-home-link" href="<?php echo get_permalink(wp_get_post_parent_id()) ?>"><i class="fa fa-home" aria-hidden="true">
+                        </i> Back to <?php echo get_the_title(wp_get_post_parent_id()) ?></a> <span class="metabox__main"><?php the_title() ?></span>
+                </p>
+            </div>
+
+        <?php }
+        ?>
+
+
         <!-- <div class="page-links">
             <h2 class="page-links__title"><a href="#">About Us</a></h2>
             <ul class="min-list">
