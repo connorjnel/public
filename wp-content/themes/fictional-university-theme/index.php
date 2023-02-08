6cb3ea9f4914da1a906dfcf1adfd5b19c4a8ffc5
connorjnel <connor.j.nel00@gmail.com>
2023-02-08 14:35:36 +0000

    blog page

--- wp-content/themes/fictional-university-theme/index.php
+++ wp-content/themes/fictional-university-theme/index.php
@@ -12,6 +12,26 @@ get_header();
     </div>
 </div>
 
+<div class="container container--narrow page-section">
+    <?php
+    while (have_posts()) {
+        the_post(); ?>
+
+        <div class="post-item">
+            <h2><a href="<?php the_permalink(); ?>" class="headline headline--medium headline--post-title"><?php the_title(); ?></a></h2>
+            <div class="metabox">
+                <p>Posted By: <?php the_author_posts_link(); ?> on: <?php the_time('d.m.y.h:i a'); ?> in <?php echo get_the_category_list(', '); ?></p>
+            </div>
+            <div class="generic-content">
+                <p><?php the_excerpt(); ?></p>
+                <p><a href="<?php echo the_permalink(); ?>" class="btn btn--blue">Continue Reading</a></p>
+            </div>
+        </div>
+
+    <?php }
+    ?>
+</div>
+
 <?php
 get_footer();
 ?>
