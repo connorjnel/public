7847ec815d0f338bd3a152cc7f799fdce2c4a795
connorjnel <connor.j.nel00@gmail.com>
2023-02-09 10:25:50 +0000

    mu plugin and custom post type

--- wp-content/mu-plugins/uni-post-types.php
+++ wp-content/mu-plugins/uni-post-types.php
@@ -6,9 +6,14 @@ function uni_post_types()
 {
     register_post_type('event', array(
         'public' => TRUE,
+        'show_in_rest' => TRUE,
         'menu_icon' => 'dashicons-calendar-alt',
         'labels' => array(
             'name' => 'Events',
+            'add_new_item' => 'Add New Event',
+            'edit_item' => 'Edit Event',
+            'all_items' => 'All Events',
+            'singular_name' => 'Name',
         )
     ));
 }
