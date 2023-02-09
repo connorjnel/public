b7240a227f8ae7a2b604f02659540c4c74289e15
connorjnel <connor.j.nel00@gmail.com>
2023-02-09 16:36:50 +0000

    custom query sorting

--- wp-content/themes/fictional-university-theme/front-page.php
+++ wp-content/themes/fictional-university-theme/front-page.php
@@ -17,10 +17,23 @@
 
             <!-- Custom loop for recent events custom post type -->
             <?php
+            $today = date('Ymd');
             $homepageEvents = new WP_Query(array(
-                'posts_per_page' => 2,
+                'posts_per_page' => -1,
                 'post_type' => 'event',
+                'meta_key' => 'event_date',
+                'orderby' => 'meta_value_num',
+                'order' => 'ASC',
+                'meta_query' => array(
+                    array(
+                        'key' => 'event_date',
+                        'compare' => '>=',
+                        'value' => $today,
+                        'type' => 'numeric',
+                    )
+                )
             ));
+
             while ($homepageEvents->have_posts()) {
                 $homepageEvents->the_post(); ?>
                 <div class="event-summary">
