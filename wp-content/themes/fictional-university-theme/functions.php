73f2e064e833aa324a81aee279e4b8b638db319c
connorjnel <connor.j.nel00@gmail.com>
2023-02-15 18:36:46 +0000

    program post type archive

--- wp-content/themes/fictional-university-theme/functions.php
+++ wp-content/themes/fictional-university-theme/functions.php
@@ -27,11 +27,19 @@ function custom_excerpt_length($length)
     return 50;
 }
 
-// Modify default query for events archive page to only show upcoming events
+// Modify default query 
 function uni_adjust_queries($query)
 {
-    $today = date('Ymd');
+    // Modify default query on program archive page
+    if (!is_admin() and is_post_type_archive('program') and $query->is_main_query()) {
+        $query->set('orderby', 'title');
+        $query->set('order', 'ASC');
+        $query->set('posts_per_page', -1);
+    }
+
+    //Modify default query on events archive page to only show upcoming events
     if (!is_admin() and is_post_type_archive('event') and $query->is_main_query()) {
+        $today = date('Ymd');
         $query->set('meta_key', 'event_date');
         $query->set('orderby', 'meta_value_num');
         $query->set('order', 'ASC');
---
+++ wp-content/themes/fictional-university-theme/single-program.php
@@ -0,0 +1,45 @@
+<!-- Single Program Post Template -->
+
+<?php
+
+get_header();
+
+while (have_posts()) {
+    the_post(); ?>
+
+    <div class="page-banner">
+        <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>);"></div>
+        <div class="page-banner__content container container--narrow">
+            <h1 class="page-banner__title"><?php the_title() ?></h1>
+            <div class="page-banner__intro">
+                <!--TODO Add call for custom field sub title -->
+                <p>DONT FORGET TO REPLACE</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="container container--narrow page-section">
+
+        <div class="post-item">
+            <h2><?php the_title(); ?></h2>
+            <div class="metabox metabox--position-up metabox--with-home-link">
+                <p><a class="metabox__blog-home-link" href="<?php echo get_post_type_archive_link('program'); ?>"><i class="fa fa-home" aria-hidden="true">
+                        </i> All Programs</a>
+                    <span class="metabox__main">
+                        <?php
+                        the_title();
+                        ?>
+                    </span>
+                </p>
+            </div>
+            <div class="generic-content">
+                <p><?php the_content(); ?></p>
+            </div>
+        </div>
+
+    </div>
+
+
+<?php }
+get_footer();
+?>
