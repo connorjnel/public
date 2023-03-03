713e74c92caa7986e3ed925f3c0e4fba6b91f560
connorjnel <connor.j.nel00@gmail.com>
2023-03-03 10:10:54 +0000

    program style tweaks

--- wp-content/themes/fictional-university-theme/single-event.php
+++ wp-content/themes/fictional-university-theme/single-event.php
@@ -41,7 +41,7 @@ while (have_posts()) {
             $relatedPrograms = get_field('related_programs');
             if ($relatedPrograms) {
                 echo '<hr class="section-break">';
-                echo '<h2 class="headline headline-medium">Related Program(s)</h2>';
+                echo '<h2 class="headline headline--medium">Related Program(s)</h2>';
                 echo '<ul class="link-list min-list">';
                 foreach ($relatedPrograms as $program) { ?>
                     <li><a href="<?php echo get_the_permalink($program) ?>"><?php echo get_the_title($program) ?></a></li>
