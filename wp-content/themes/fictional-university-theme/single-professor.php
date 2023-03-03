3c5176fbc030a58a3e9ebb52d245451d068610ea
connorjnel <connor.j.nel00@gmail.com>
2023-03-03 10:35:24 +0000

    add professor post type

---
+++ wp-content/themes/fictional-university-theme/single-professor.php
@@ -0,0 +1,47 @@
+<!-- Single Event Post Template -->
+
+<?php
+
+get_header();
+
+while (have_posts()) {
+    the_post(); ?>
+
+    <div class="page-banner">
+        <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>);"></div>
+        <div class="page-banner__content container container--narrow">
+            <h1 class="page-banner__title"><?php the_title() ?></h1>
+            <div class="page-banner__intro">
+                <!--TODO Add call for custom field sub title -->
+                <p>DONT FORGET TO REPLACE</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="container container--narrow page-section">
+
+        <div class="post-item">
+            <div class="generic-content">
+                <p><?php the_content(); ?></p>
+            </div>
+
+            <?php
+            $relatedPrograms = get_field('related_programs');
+            if ($relatedPrograms) {
+                echo '<hr class="section-break">';
+                echo '<h2 class="headline headline--medium">Subject(s) Taught</h2>';
+                echo '<ul class="link-list min-list">';
+                foreach ($relatedPrograms as $program) { ?>
+                    <li><a href="<?php echo get_the_permalink($program) ?>"><?php echo get_the_title($program) ?></a></li>
+            <?php }
+                echo '</ul>';
+            }
+            ?>
+        </div>
+
+    </div>
+
+
+<?php }
+get_footer();
+?>
