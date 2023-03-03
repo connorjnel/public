fe370cdefcc901581dfdb3837d45eb4cb2145bc8
connorjnel <connor.j.nel00@gmail.com>
2023-03-03 13:31:38 +0000

    page banner acf

--- wp-content/themes/fictional-university-theme/functions.php
+++ wp-content/themes/fictional-university-theme/functions.php
@@ -1,5 +1,25 @@
 <?php
 
+function pageBanner()
+{
+    // PHP logic will live here
+?>
+
+    <div class="page-banner">
+        <div class="page-banner__bg-image" style="background-image: 
+        url(<?php $pageBannerImage = get_field('page_banner_background_image');
+            echo $pageBannerImage['sizes']['pageBanner']; ?>);">
+        </div>
+        <div class="page-banner__content container container--narrow">
+            <h1 class="page-banner__title"><?php the_title() ?></h1>
+            <div class="page-banner__intro">
+                <p><? the_field('page_banner_subtitle'); ?></p>
+            </div>
+        </div>
+    </div>
+
+<?php }
+
 // Load stylesheets, js scripts, font files
 function uni_theme_files()
 {
@@ -20,6 +40,7 @@ function uni_features()
     // Add custom thumbail sizes
     add_image_size('professorLandscape', 400, 260, true);
     add_image_size('professorPortrait', 480, 650, true);
+    add_image_size('pageBanner', 1500, 350, true);
     // Register menu locations, lets it be edited in WP backend
     register_nav_menu('headerMenuLocation', 'Header Main Menu');
     register_nav_menu('footerMenu1', 'Footer Menu 1');
