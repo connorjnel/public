1fa2cb316e28821667afdef15eefae2c8d82e00c
connorjnel <connor.j.nel00@gmail.com>
2023-03-21 09:43:46 +0000

    custom user roles

--- wp-content/themes/fictional-university-theme/header.php
+++ wp-content/themes/fictional-university-theme/header.php
@@ -29,8 +29,8 @@
           ?>
         </nav>
         <div class="site-header__util">
-          <a href="#" class="btn btn--small btn--orange float-left push-right">Login</a>
-          <a href="#" class="btn btn--small btn--dark-orange float-left">Sign Up</a>
+          <a href="<?php echo esc_url(site_url('/wp-login.php')); ?>" class="btn btn--small btn--orange float-left push-right">Login</a>
+          <a href="<?php echo esc_url(site_url('/wp-signup.php')); ?>" class="btn btn--small btn--dark-orange float-left">Sign Up</a>
           <a href="<?php echo esc_url(site_url('/search')); ?>" class="js-search-trigger search-trigger"><i class="fa fa-search" aria-hidden="true"></i></a>
         </div>
       </div>
